efedab2781c4afa68a015b9bb3441b668e2aea70
Vivek <vivekkumar.oct17@gmail.com>
2022-06-02 16:02:55 +0530

    isSoetd

---
+++ 25_check_sorted.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isSorted(int arr[],int size){
+
+    if(size==0|| size==1){
+        return true;
+    }
+
+    if(arr[0]>arr[1]){
+        return false;
+    }
+    else{
+        bool remainingPart = isSorted(arr+1, size-1); 
+        return remainingPart;
+    }
+
+}
+
+int main(){
+
+    int arr [8]={1,2,3,4,5,6,7,8};
+
+    int size = 8;
+
+
+    bool ans = isSorted(arr,size);
+    if(ans){
+        cout<<"TRUE";
+    }
+    else{
+        cout<<"False";
+    }
+return 0;
+}

14d79c6d2dc72e18b6067446147c6c4b0b177df5
Vivek <vivekkumar.oct17@gmail.com>
2022-06-09 19:42:20 +0530

    --

--- 33_linked_list.cpp
+++ 33_linked_list.cpp
@@ -12,17 +12,6 @@ class Node {
             this->next = NULL;
         }
 };
-void insertAtHead(Node* &head, int d){
-
-    // create new node
-    Node* temp = new Node(d);  
-    //point temp next to prev head
-    temp->next = head;
-    // make head point to temp's head
-    head = temp;
-
-}
-
 void printList(Node* &head){
 
     Node* temp = head;
@@ -35,11 +24,46 @@ void printList(Node* &head){
     cout<<endl;
     
 }
+//                   Insertion at HEAD
+void insertAtHead(Node* &head, int d){
+
+    // create new node
+    Node* temp = new Node(d);  
+    //point temp next to prev head
+    temp->next = head;
+    // make head point to temp's head
+    head = temp;
+
+}
+//                      Insretion at TAIL
+void insertAtTail(Node* &tail, int d){  
+    // create new node
+    Node* temp = new Node(d);  
+
+    if(tail->next==NULL){
+        tail-> next=temp;
+    tail=temp;
+    temp->next=NULL;
+    }
+
+    while(tail->next!=NULL){
+        tail=tail->next;
+    }
+    
+
+}
 
 int main(){ 
 
-    Node* node1 = new Node(20);
+    Node* node1 = new Node(10);
+    // Node* node2 = new Node(20);
+    // Node* node3 = new Node(30);
+    // Node* node4 = new Node(40);
 
+
+    // node1->next=node2;
+    // node2->next=node3;
+    // node3->next=node4;
     // Node* node1 =new Node;
 
     // node1->data = 20;
@@ -47,13 +71,21 @@ int main(){
 
     // cout<<node1->data<<endl;
     // cout<<node1->next<<endl;
-
     Node* head = node1;
+    Node* tail = node1;
     printList(head);
 
-    cout<<endl;
-    insertAtHead(head,30);
+    // cout<<endl;
+    // insertAtHead(head,99);
+    // printList(head);
+
+    // cout<<endl;
+    // insertAtHead(head,79);
+    // printList(head);
+
+    insertAtTail(tail,60);
     printList(head);
+
 
  
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
-sayDigits(n,arr);
+
+    
